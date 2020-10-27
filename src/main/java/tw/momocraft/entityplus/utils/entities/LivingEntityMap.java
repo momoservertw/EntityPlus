83012bbdfb8c7a5051f6c7f23a3bd771a88b524f
Momo <momoservertw@gmail.com>
2020-10-28 04:52:12 +0800

    Working on version 1.2.0 za

--- src/main/java/tw/momocraft/entityplus/utils/entities/LivingEntityMap.java
+++ src/main/java/tw/momocraft/entityplus/utils/entities/LivingEntityMap.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.UUID;
 
 public class LivingEntityMap {
-    private Map<UUID, Pair<String, String>> mobsMap;
+    private final Map<UUID, Pair<String, String>> mobsMap;
 
     public LivingEntityMap() {
         mobsMap = new HashMap<>();
