9673323144989ee47cdb9e85d1aeb789a0ea009d
Momo <momoservertw@gmail.com>
2020-11-26 18:03:23 +0800

    Working on version 1.2.4 g
    Change tab complete.

--- src/main/java/tw/momocraft/entityplus/utils/TabComplete.java
+++ src/main/java/tw/momocraft/entityplus/utils/TabComplete.java
@@ -3,10 +3,12 @@ package tw.momocraft.entityplus.utils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 import tw.momocraft.entityplus.handlers.PermissionsHandler;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,19 +18,40 @@ public class TabComplete implements TabCompleter {
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         final List<String> completions = new ArrayList<>();
         final List<String> commands = new ArrayList<>();
-        if (args.length == 2 && args[0].equalsIgnoreCase("help") && PermissionsHandler.hasPermission(sender, "entityplus.use"))
-            return null;
+        /*
+        Collection<?> playersOnlineNew;
+        Player[] playersOnlineOld;
+        try {
+            if (Bukkit.class.getMethod("getOnlinePlayers").getReturnType() == Collection.class) {
+                if (Bukkit.class.getMethod("getOnlinePlayers").getReturnType() == Collection.class) {
+                    playersOnlineNew = ((Collection<?>) Bukkit.class.getMethod("getOnlinePlayers", new Class<?>[0]).invoke(null, new Object[0]));
+                    for (Object objPlayer : playersOnlineNew) {
+                        commands.add(((Player) objPlayer).getName());
+                    }
+                }
+            } else {
+                playersOnlineOld = ((Player[]) Bukkit.class.getMethod("getOnlinePlayers", new Class<?>[0]).invoke(null, new Object[0]));
+                for (Player player : playersOnlineOld) {
+                    commands.add(player.getName());
+                }
+            }
+        } catch (Exception e) {
+            ServerHandler.sendDebugTrace(e);
+        }
+         */
 
-        if (args.length == 1) {
-            if (PermissionsHandler.hasPermission(sender, "entityplus.use")) {
-                commands.add("help");
-            }
-            if (PermissionsHandler.hasPermission(sender, "entityplus.command.reload")) {
-                commands.add("reload");
-            }
-            if (PermissionsHandler.hasPermission(sender, "entityplus.command.version")) {
-                commands.add("version");
-            }
+        switch (args.length) {
+            case 1:
+                if (PermissionsHandler.hasPermission(sender, "entityplus.use")) {
+                    commands.add("help");
+                }
+                if (PermissionsHandler.hasPermission(sender, "entityplus.command.reload")) {
+                    commands.add("reload");
+                }
+                if (PermissionsHandler.hasPermission(sender, "entityplus.command.version")) {
+                    commands.add("version");
+                }
+                break;
         }
         StringUtil.copyPartialMatches(args[(args.length - 1)], commands, completions);
         Collections.sort(completions);
